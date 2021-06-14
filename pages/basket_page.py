7f9ee48a2bc650cc9ed7167fcde24f95eeda8dd3
farxaddoctor <42151772+farxaddoctor@users.noreply.github.com>
2021-06-14 10:52:23 +0400

    Update basket_page.py

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -2,17 +2,17 @@ from .base_page import BasePage
 from .locators import LoginPageLocators
 from .locators import CartPageLocators
 
-class CartPage(BasePage):
-    def cart_should_be_empty(self):
-        self.shouldnt_be_any_product_in_a_cart()
-        self.should_be_empty_cart_text_on_basket_page()
+class BasketPage(BasePage):
+    def basket_should_be_empty(self):
+        self.shouldnt_be_any_product_in_a_basket()
+        self.should_be_empty_basket_text_on_basket_page()
 
-    def shouldnt_be_any_product_in_a_cart(self):
+    def shouldnt_be_any_product_in_a_basket(self):
         substring = CartPageLocators.SUBSTRING_BASKET_EN_GB
         assert substring in self.browser.find_element(*CartPageLocators.BASKET_EMPTY).text, "No message of empty basket!"
 
-    def should_be_empty_cart_text_on_basket_page(self):
+    def should_be_empty_basket_text_on_basket_page(self):
         assert self.is_not_element_present(*CartPageLocators.BASKET_NOT_EMPTY), "Basket is not empty, but it should!"
 
-    def should_be_not_cart_text_on_basket_page(self):
+    def should_be_not_basket_text_on_basket_page(self):
         assert self.is_element_present(*CartPageLocators.BASKET_NOT_EMPTY), "Basket is empty, but it shouldn't!"
